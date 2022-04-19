e31c6198ac03cbfc41bde09f5d44b235afa39aa9
Arkadiusz Wtykło <arek.wtyklo@arteneoit.pl>
2022-04-19 16:17:39 +0200

    feat: Add DebugToolbarReplaceSubscriber

---
+++ src/EventSubscriber/DebugToolbarReplaceSubscriber.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Carve\ApiBundle\EventSubscriber;
+
+use Symfony\Component\DependencyInjection\Attribute\When;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+#[When(env: 'dev')]
+class DebugToolbarReplaceSubscriber implements EventSubscriberInterface
+{
+    public function onKernelResponse(ResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        $response = $event->getResponse();
+        $response->headers->set('Symfony-Debug-Toolbar-Replace', 1);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::RESPONSE => [
+                ['onKernelResponse', 0],
+            ],
+        ];
+    }
+}
