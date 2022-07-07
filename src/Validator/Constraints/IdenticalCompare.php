7c1d61e1aa50a9151d33e982661b42267bd0168e
Arkadiusz Wtykło <arek.wtyklo@arteneoit.pl>
2022-07-08 00:09:09 +0200

    feat(i-2): IdenticalCompare and PasswordIdenticalCompare validators

---
+++ src/Validator/Constraints/IdenticalCompare.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Carve\ApiBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+#[\Attribute(\Attribute::TARGET_CLASS)]
+class IdenticalCompare extends Constraint
+{
+    public $message = 'validation.notIdentical';
+    public $propertyPath1;
+    public $propertyPath2;
+
+    public function getRequiredOptions()
+    {
+        return [
+            'propertyPath1',
+            'propertyPath2',
+        ];
+    }
+}
---
+++ src/Validator/Constraints/IdenticalCompareValidator.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Carve\ApiBundle\Validator\Constraints;
+
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class IdenticalCompareValidator extends ConstraintValidator
+{
+    private $propertyAccessor;
+
+    public function validate($protocol, Constraint $constraint)
+    {
+        $value1 = $this->getPropertyAccessor()->getValue($protocol, $constraint->propertyPath1);
+        $value2 = $this->getPropertyAccessor()->getValue($protocol, $constraint->propertyPath2);
+
+        if ($value1 != $value2) {
+            $this->context->buildViolation($constraint->message)->atPath($constraint->propertyPath1)->addViolation();
+            $this->context->buildViolation($constraint->message)->atPath($constraint->propertyPath2)->addViolation();
+        }
+    }
+
+    private function getPropertyAccessor(): PropertyAccessorInterface
+    {
+        if (null === $this->propertyAccessor) {
+            $this->propertyAccessor = PropertyAccess::createPropertyAccessor();
+        }
+
+        return $this->propertyAccessor;
+    }
+}
