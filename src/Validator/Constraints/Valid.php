6100bbd0b627949acf1e831b0b7b8dbc2f1d3ca5
Arek Wtykło <arek.wtyklo@arteneo.pl>
2022-11-04 16:25:32 +0100

    feat: Add Assert\Valid

---
+++ src/Validator/Constraints/Valid.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Carve\ApiBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * This is direct use of existing Valid assert. Created only for developer convienience.
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
+class Valid extends Assert\Valid
+{
+    public function validatedBy(): string
+    {
+        return Assert\ValidValidator::class;
+    }
+}
